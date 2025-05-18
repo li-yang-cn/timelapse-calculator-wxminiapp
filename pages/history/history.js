76664401a86a119ae11888b4b2806b6a71210691
Yang Li <i@liyang.tech>
2025-05-18 22:41:09 +0800

    sharing function
    完成分享功能，表格对齐

--- pages/history/history.js
+++ pages/history/history.js
@@ -51,4 +51,62 @@ Page({
             title: '延时摄影计算器·快速计算拍摄参数'
         }
     },
+    data: {
+        showShareImage: false,
+        shareImagePath: '',
+    },
+    onShareRecord(e) {
+        const index = e.currentTarget.dataset.index;
+        const record = this.data.history[index];
+        this.drawShareImage(record);
+    },
+    closeShareImage() {
+        this.setData({ showShareImage: false });
+    },
+    drawShareImage(record) {
+        const ctx = wx.createCanvasContext('shareCanvas', this);
+        const width = 600;
+        const height = 400;
+        const topOffset = 30;
+        // 背景
+        ctx.setFillStyle('#fff');
+        ctx.fillRect(0, 0, width, height);
+        // 上半部分表格
+        ctx.setFontSize(22);
+        ctx.setFillStyle('#222');
+        ctx.fillText('拍摄时长', 30, 50 + topOffset);
+        ctx.fillText(record.duration + ' 分', 180, 50 + topOffset);
+        ctx.fillText('成片时长', 320, 50 + topOffset);
+        ctx.fillText(record.finalDuration + ' 秒', 470, 50 + topOffset);
+        ctx.fillText('帧速率', 30, 100 + topOffset);
+        ctx.fillText(record.frameRate + ' fps', 180, 100 + topOffset);
+        ctx.fillText('拍摄间隔', 320, 100 + topOffset);
+        ctx.fillText(record.interval + ' 秒', 470, 100 + topOffset);
+        ctx.fillText('总张数', 30, 150 + topOffset);
+        ctx.fillText(record.totalFrames + ' 张', 180, 150 + topOffset);
+        // 下半部分
+        ctx.setFontSize(18);
+        ctx.setFillStyle('#444');
+        ctx.fillText('延时摄影参数由"延时摄影计算器"', 30, 250 + topOffset);
+        ctx.fillText('小程序计算得出', 30, 280 + topOffset);
+        // 绘制二维码
+        ctx.drawImage('/images/qr_code.jpg', 400, 200 + topOffset, 160, 160);
+        ctx.draw(false, () => {
+            wx.canvasToTempFilePath({
+                canvasId: 'shareCanvas',
+                width,
+                height,
+                fileType: 'jpg',
+                success: (res) => {
+                    this.setData({
+                        shareImagePath: res.tempFilePath,
+                        showShareImage: true
+                    });
+                },
+                fail: (err) => {
+                    wx.showToast({ title: '生成图片失败', icon: 'none' });
+                }
+            }, this);
+        });
+    },
 });
