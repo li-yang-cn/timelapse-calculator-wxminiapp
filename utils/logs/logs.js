9aa6b6a2cd36fa0e647b1d9acdc842a939869cb3
yang.li <yang.li@liyang.tech>
2026-05-26 23:44:02 +0800

    Remove startup SDK calls causing devtools errors

--- utils/logs/logs.js
+++ utils/logs/logs.js
@@ -1,33 +1,20 @@
-const util = require('../util.js')
-
-var log = wx.getRealtimeLogManager ? wx.getRealtimeLogManager() : null
-
 module.exports = {
   debug() {
-    if (!log) return
-    log.debug.apply(log, arguments)
+    console.debug.apply(console, arguments)
   },
   info() {
-    if (!log) return
-    log.info.apply(log, arguments)
-    console.log(arguments)
+    console.info.apply(console, arguments)
   },
   warn() {
-    if (!log) return
-    log.warn.apply(log, arguments)
+    console.warn.apply(console, arguments)
   },
   error() {
-    if (!log) return
-    log.error.apply(log, arguments)
+    console.error.apply(console, arguments)
   },
   setFilterMsg(msg) { // 从基础库2.7.3开始支持
-    if (!log || !log.setFilterMsg) return
-    if (typeof msg !== 'string') return
-    log.setFilterMsg(msg)
+    return msg
   },
   addFilterMsg(msg) { // 从基础库2.8.1开始支持
-    if (!log || !log.addFilterMsg) return
-    if (typeof msg !== 'string') return
-    log.addFilterMsg(msg)
+    return msg
   }
 }
